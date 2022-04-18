79f94b7556bf8c244affa8e04a687497a670b278
forhadbinamir <forhadbinamir@gmail.com>
2022-04-18 07:34:58 +0300

    added a google authentication

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,10 @@
 import React, { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../Hooks/Firebase.init';
 import Loading from '../../Shared/Loading/Loading';
+import google from '../../images/Google/google.png'
 const Register = () => {
     const [check, setCheck] = useState(false)
     const [
@@ -12,6 +13,11 @@ const Register = () => {
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    // sign in with google 
+    const [signInWithGoogle, userGoogle, loadingGoogle, googleError] = useSignInWithGoogle(auth);
+
+
     const navigate = useNavigate()
 
     // input value cast here 
@@ -32,19 +38,26 @@ const Register = () => {
         createUserWithEmailAndPassword(email, password)
 
     }
-    if (user) {
+    if (user || userGoogle) {
         navigate('/login')
     }
-    if (loading) {
+    if (loading || loadingGoogle) {
         return <Loading></Loading>
     }
     let errorText;
-    if (error) {
+    if (error || googleError) {
         errorText = <p className='text-danger'>Error: {error?.message}</p>
     }
+
     return (
-        <div>
-            <div className="form-container">
+        <div className='flex justify-center align-items-center p-5'>
+            <div className='w-50 m-3 mx-auto'>
+                <div onClick={() => signInWithGoogle()} className='flex justify-center items-center rounded-lg bg-red-200 border p-20'>
+                    <img className='w-[50px] mr-2' src={google} alt="" />
+                    <h2>Sign up with google</h2>
+                </div>
+            </div>
+            <div className="form-container w-50 m-3">
                 <div className="title-form">
                     <h2 className='text-center my-6 text-4xl font-bold border-bottom w-[50%] pb-2 mx-auto'>Register <span className='text-red-600'>Form</span></h2>
                 </div>
@@ -71,10 +84,15 @@ const Register = () => {
 
 
                     {errorText}
-                    <input disabled={!check} className='bg-red-600 text-white font-bold hover:bg-red-800' type="submit" value="Register" />
+                    <input disabled={!check} className='mt-2 bg-red-600 text-white font-bold hover:bg-red-800' type="submit" value="Register" />
                 </form>
                 <div>
                     <p className='text-center mt-3'>Already have an account <Link to='/login' className='text-red-600 font-bold hover:text-red-800 cursor-pointer no-underline'>Please Login</Link> </p>
+                </div>
+                <div className='flex justify-center align-items-center'>
+                    <div className='h-[1px] w-40 bg-red-600'></div>
+                    <p className='mt-2 mx-2'>or</p>
+                    <div className='h-[1px] w-40 bg-red-600'></div>
                 </div>
             </div>
         </div>
