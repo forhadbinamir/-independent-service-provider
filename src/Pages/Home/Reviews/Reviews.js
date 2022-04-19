f93107d360ac7dccace09a9b95e3fa444f4142dd
forhadbinamir <forhadbinamir@gmail.com>
2022-04-19 16:10:08 +0300

    update responsive menu

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,76 +1,19 @@
-// import React, { useRef, useState } from "react";
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useEffect, useState } from 'react';
 
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/effect-coverflow";
-// import "swiper/css/pagination";
+const Reviews = () => {
+    const [review, setReview] = useState([])
 
+    useEffect(() => {
 
-// // import required modules
-// import { EffectCoverflow, Pagination } from "swiper";
+    }, [])
+    return (
+        <div>
+            <div>
+                <h2>My Client Reviews</h2>
+            </div>
 
-// const Reviews = () => {
-//     return (
-//         <>
-//             <Swiper
-//                 effect={"coverflow"}
-//                 grabCursor={true}
-//                 centeredSlides={true}
-//                 slidesPerView={"auto"}
-//                 coverflowEffect={{
-//                     rotate: 50,
-//                     stretch: 0,
-//                     depth: 100,
-//                     modifier: 1,
-//                     slideShadows: true,
-//                 }}
-//                 pagination={true}
-//                 modules={[EffectCoverflow, Pagination]}
-//                 className="mySwiper"
-//             >
-//                 <SwiperSlide>
-//                     <div className="">
-//                         <img className="h-[400px]" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-//                         <h1>heoo</h1>
-//                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum minus voluptas, labore obcaecati consequuntur quos culpa aperiam. Omnis, aut soluta eaque fuga eligendi asperiores atque possimus porro suscipit nisi cum?</p>
-//                     </div>
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <div className="">
-//                         <img className="h-[400px]" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-//                         <h1>heoo</h1>
-//                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum minus voluptas, labore obcaecati consequuntur quos culpa aperiam. Omnis, aut soluta eaque fuga eligendi asperiores atque possimus porro suscipit nisi cum?</p>
-//                     </div>
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <div className="">
-//                         <img className="h-[400px]" src="https://swiperjs.com/demos/images/nature-1.jpg" />
-//                         <h1>heoo</h1>
-//                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum minus voluptas, labore obcaecati consequuntur quos culpa aperiam. Omnis, aut soluta eaque fuga eligendi asperiores atque possimus porro suscipit nisi cum?</p>
-//                     </div>
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <img className=" h-[400px]" src="https://swiperjs.com/demos/images/nature-4.jpg" />
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-//                 </SwiperSlide>
-//                 <SwiperSlide>
-//                     <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-//                 </SwiperSlide>
-//             </Swiper>
-//         </>
-//     );
-// }
-// export default Reviews;
+        </div>
+    );
+};
+
+export default Reviews;
