ba51b25af26a3bae33cf4eef296d13d8e9638c25
forhadbinamir <forhadbinamir@gmail.com>
2022-04-19 15:51:33 +0300

    update responsive

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About/About';
+import Reviews from './Reviews/Reviews';
 import Services from './Services';
 import Slider from './Slider';
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Slider></Slider>
             <About></About>
             <Services></Services>
+
         </div>
     );
 };
